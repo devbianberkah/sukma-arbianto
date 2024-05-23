d33dcf86d2656d32d67e88d65c8d2fdaabcba265
Sukma Arbianto <sukmasuk2006@gmail.com>
2024-05-23 16:56:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav id="desktop-nav">
+          <div className='logo'>Sukma</div>
+          <div>
+              <ul className="nav-links">
+                <li><a href='#about'>About</a></li>
+                <li><a href='#experience'>Experience</a></li>
+                <li><a href='#project'>Project</a></li>
+                <li><a href='#contact'>Contact</a></li>
+              </ul>
+          </div>
+      </nav>
     </div>
   );
 }
