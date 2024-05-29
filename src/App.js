add34ae6502b88f4815e205851cac0b70dca6c36
Sukma Arbianto <sukmasuk2006@gmail.com>
2024-05-29 21:23:38 +0700

    add some assets

--- src/App.js
+++ src/App.js
@@ -24,6 +24,26 @@ function App() {
         <div className='logo'>Sukma</div>
         <HeaderMenu />
       </nav>
+        <section id="profile">
+          <div className="section__pic-container">
+            <img src={window.location.origin+'/img/profil.jpg'} alt='profile-img'></img>
+          </div>
+          <div className='section__text'>
+            <p className='section__text_p1'>Hello, I'm</p>
+            <h1 className='title'>Sukma</h1>
+            <p className='section__text_p2'>Frontend Developer</p>
+            <div className='btn-container'>
+              <a className='btn btn-color-2' target='_blank' href='https://drive.google.com/file/d/1iJ7ZprOdJn9PSxVtvjA7igF7RtQnb8lv/view?usp=sharing' download='cv'>
+              Download CV</a> 
+              <button className='btn btn-color-1'>
+              Contact Info
+              </button>
+            </div>
+            <div id="socials-container">
+               <img src={window.location.origin+'/assets/linkedin.png'}></img>
+            </div>
+          </div>
+        </section>
     </div>
   );
 }
